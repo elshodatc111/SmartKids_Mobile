e778c9ba7e7cc0b6906c988dcc61ca9787fe54b1
Elshod Musurmonov <elshodatc1116@gmail.com>
2026-02-10 15:56:57 +0500

    Save

---
+++ smart_kids_app_end/lib/screen/xodim/xodim_show_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class XodimShowPage extends StatefulWidget {
+  final int id;
+  const XodimShowPage({super.key, required this.id});
+
+  @override
+  State<XodimShowPage> createState() => _XodimShowPageState();
+}
+
+class _XodimShowPageState extends State<XodimShowPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
